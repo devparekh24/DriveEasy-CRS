a19da7c35f80636782626128f67071514a7edbf9
devparekh24 <devparekh2424@gmail.com>
2024-01-22 19:14:00 +0530

    created userController, userRoutes & errorController

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+const errorController = require('./controller/errorController')
 const authRouter = require('./routes/authRoutes')
+const userRouter = require('./routes/userRoutes')
 
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/', authRouter)
+app.use('/users', userRouter)
+
+app.use(errorController)
 
 module.exports = app
