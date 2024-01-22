2ec2e2a1c935cb09d2f44a152d988a340023d8df
devparekh24 <devparekh2424@gmail.com>
2024-01-22 17:10:45 +0530

    project setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const app = require('./app')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(db).then(() => console.log('DataBase Connected Successfully...'));
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Connection established with the sever on port : ${port}`))
