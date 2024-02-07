c6c144ac2f78d9071a9518c152c3d48f84b22ea9
devparekh24 <devparekh2424@gmail.com>
2024-02-07 11:15:50 +0530

    commentout persist state management

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -7,33 +7,33 @@ import { carApi } from "../services/carApi";
 import addressReducer from "../slices/addressSlice";
 import orderReducer from "../slices/orderSlice";
 import { orderApi } from "../services/orderApi";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-    key: 'root',
-    storage,
-}
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+// }
 
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-const persistedCarReducer = persistReducer(persistConfig, carReducer);
-const persistedAddressReducer = persistReducer(persistConfig, addressReducer);
-const persistedOrderReducer = persistReducer(persistConfig, orderReducer);
+// const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+// const persistedCarReducer = persistReducer(persistConfig, carReducer);
+// const persistedAddressReducer = persistReducer(persistConfig, addressReducer);
+// const persistedOrderReducer = persistReducer(persistConfig, orderReducer);
 
 export const store = configureStore({
     reducer: {
-        auth: persistedAuthReducer,
+        auth: authReducer,
         [authApi.reducerPath]: authApi.reducer,
-        car: persistedCarReducer,
+        car: carReducer,
         [carApi.reducerPath]: carApi.reducer,
-        address: persistedAddressReducer,
-        order: persistedOrderReducer,
+        address: addressReducer,
+        order: orderReducer,
         [orderApi.reducerPath]: orderApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware, carApi.middleware, orderApi.middleware)
 })
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
